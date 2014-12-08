f1a873120ad22ed6989d7730cd92038bdb7529c0
Simon Larsen <simonhffh@gmail.com>
2014-12-08 13:15:47 +0100

    Added backgrounds to gallery scene

--- Background.lua
+++ Background.lua
@@ -8,11 +8,7 @@ function Background:initialize(id)
 	self.id = id
 	self.state = 1
 
-	self.image = Resources.static:getImage("backgrounds.png")
-	self.quads = {}
-	for i=1, Background.static.NUM_BACKGROUNDS do
-		self.quads[i] = love.graphics.newQuad((i-1)*152, 0, 152, 184, 760, 184)
-	end
+	self.image = Resources.static:getImage("background"..self.id..".png")
 end
 
 function Background:update(dt)
@@ -32,6 +28,7 @@ function Background:update(dt)
 			if self.id > Background.static.NUM_BACKGROUNDS then
 				self.id = 1
 			end
+			self.image = Resources.static:getImage("background"..self.id..".png")
 		end
 	end
 end
--- GalleryScene.lua
+++ GalleryScene.lua
@@ -1,15 +1,39 @@
 local GalleryScene = class("GalleryScene", Scene)
 
+GalleryScene.static.MAX_SCROLL = 850
+
 function GalleryScene:initialize()
 	Scene.initialize(self)
 
 	self.scroll = 0
 	self.day = Preferences.static:get("days")
 	self.background = Resources.static:getImage("background.png")
+	self.canvas = Resources.static:getImage("canvas.png")
+	self.quad_background = love.graphics.newQuad(12, 10, 120, 160, 152, 184)
+
+	self:loadDay()
+end
+
+function GalleryScene:loadDay()
+	self.customer_order = Preferences.static:get(string.format("day_%d_customer_order", self.day))
+	self.background_order = Preferences.static:get(string.format("day_%d_background_order", self.day))
 
 	self.paintings = {}
 	for i=1,5 do
 		self.paintings[i] = love.graphics.newImage(string.format("painting_%d_%d.png", self.day, i))
+	end
+
+	self.customers = {}
+	self.quads_customers = {}
+	self.backgrounds = {}
+
+	for i=1,5 do
+		self.customers[i] = Resources.static:getImage("customer"..self.customer_order[i]..".png")
+		local w = self.customers[i]:getWidth()
+		local h = self.customers[i]:getHeight()
+		self.quads_customers[i] = love.graphics.newQuad(w/2-60, h-230, 120, 160, w, h)
+
+		self.backgrounds[i] = Resources.static:getImage("background"..self.background_order[i]..".png")
 	end
 end
 
@@ -19,7 +43,14 @@ function GalleryScene:update(dt)
 	if mx >= WIDTH/2-16 and mx <= WIDTH/2+16
 	and my >= HEIGHT-32 and my <= HEIGHT then
 		if Mouse.static:isDown("l") then
-			self.scroll = self.scroll + dt*100
+			self.scroll = math.min(GalleryScene.static.MAX_SCROLL, self.scroll + dt*200)
+		end
+	end
+
+	if mx >= WIDTH/2-16 and mx <= WIDTH/2+16
+	and my <= 32 then
+		if Mouse.static:isDown("l") then
+			self.scroll = math.max(0, self.scroll - dt*200)
 		end
 	end
 end
@@ -35,12 +66,23 @@ function GalleryScene:gui()
 	love.graphics.translate(0, -self.scroll)
 
 	for i=1,5 do
-		love.graphics.draw(self.paintings[i], 20, i*200)
+		love.graphics.draw(self.canvas, 20, i*180)
+		love.graphics.draw(self.paintings[i], 20, i*180)
+
+		love.graphics.draw(self.canvas, 180, i*180)
+		love.graphics.draw(self.backgrounds[i], self.quad_background, 180, i*180)
+		love.graphics.draw(self.customers[i], self.quads_customers[i], 180, i*180)
 	end
 
 	love.graphics.pop()
 
-	love.graphics.rectangle("fill", WIDTH/2-16, HEIGHT-32, 32, 32)
+	if self.scroll > 0 then
+		love.graphics.rectangle("fill", WIDTH/2-16, 0, 32, 32)
+	end
+
+	if self.scroll < GalleryScene.static.MAX_SCROLL then
+		love.graphics.rectangle("fill", WIDTH/2-16, HEIGHT-32, 32, 32)
+	end
 end
 
 return GalleryScene
